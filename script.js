8ad8903059b789d3f3f3cca7fe269b0982cfdcb8
Leon Khidilov <khidilov@gmail.com>
2025-09-18 19:58:37 +0400

    fixed type with defining rootMargin in intersection observer

--- script.js
+++ script.js
@@ -194,14 +194,13 @@ const navHeight = nav.getBoundingClientRect().height;
 // Intersection Observer API tədbiqi
 const stickyNav = entries => {
   const [entry] = entries;
-  console.log(entry);
   if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
 };
 const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
   threshold: 0,
-  rootMargin: `-  ${navHeight}px`,
+  rootMargin: `-${navHeight}px`,
 });
 headerObserver.observe(header);
 
